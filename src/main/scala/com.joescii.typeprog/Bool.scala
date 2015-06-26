5920fa21790fe7fb2b563f674278632b8733fe73
Leonardo Montenegro <leonardo.montenegro@chaotive.cl>
2015-06-26 15:00:21 -0300

    Step 1 done

--- src/main/scala/com.joescii.typeprog/Bool.scala
+++ src/main/scala/com.joescii.typeprog/Bool.scala
@@ -1,6 +1,14 @@
 package com.joescii.typeprog
 
-sealed trait Bool
-sealed trait True extends Bool
-sealed trait False extends Bool
-
+sealed trait Bool {
+  type Not <: Bool
+  type Or[That <: Bool] <: Bool
+}
+sealed trait True extends Bool {
+  override type Not = False
+  override type Or[That <: Bool] = True
+}
+sealed trait False extends Bool {
+  override type Not = True
+  override type Or[That <: Bool] = That
+}
